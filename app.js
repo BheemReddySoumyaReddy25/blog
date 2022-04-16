15183428857c6ea5f4ae1bea8bb56c83c7dc7cdc
BHEEMREDDY SOUMYA REDDY <68443124+BheemReddySoumyaReddy25@users.noreply.github.com>
2022-04-16 16:08:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 require('dotenv/config');
-mongoose.connect('mongodb+srv://admin_soumya:hahaha@cluster0.20mmm.mongodb.net/Blog1DB?retryWrites=true&w=majority',{ useUnifiedTopology: true ,useNewUrlParser: true});
+mongoose.connect('mongodb+srv://admin.mongodb.net/Blog1DB?retryWrites=true&w=majority',{ useUnifiedTopology: true ,useNewUrlParser: true});
 const postSchema = {
 
     name: String,
